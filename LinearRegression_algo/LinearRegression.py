97b21ed6727f299e6fae0e6845f858da01678fea
Maxime Kotur <maxkotur@gmail.com>
2023-08-30 16:51:37 -0400

    fit method done

--- LinearRegression_algo/LinearRegression.py
+++ LinearRegression_algo/LinearRegression.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class LinearRegression:
     
@@ -10,7 +10,20 @@ class LinearRegression:
         
         
     def fit(self, X, y):
-        pass
+        # initial params
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+        
+        for _ in range(self.iter):
+            y_predicted = np.dot(X, self.weights) + self.bias # y_hat = wx + b
+            
+            dw = (1/n_samples) * np.dot(X.T, (y_predicted - y)) # derivative of weight
+            db = (1/n_samples) * np.sum(y_predicted - y) # derivated of bias
+
+            # update weights and bias
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
     
     def predict(self, X):
-        pass
+        y_predicted = np.dot(X, self.weights) + self.bias
