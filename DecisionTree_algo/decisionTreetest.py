6057aca6e1776635b1dc68f075184b5ec6aae6da
Maxime Kotur <maxkotur@gmail.com>
2023-09-04 18:15:31 -0400

    entropy commit

--- DecisionTree_algo/decisionTreetest.py
+++ DecisionTree_algo/decisionTreetest.py
@@ -12,6 +12,7 @@
 # When we reach the leaf node, return the most common class label
 
 import numpy as np
+from collections import Counter
 
 def entropy(y):
     hist = np.bincount(y)
@@ -35,5 +36,72 @@ class Node:
 
 class DecisionTree:
     
-    def __init__(self, min_sample_split=2, max_depth=100, n_f):
+    def __init__(self, min_samples_split=2, max_depth=100, n_features=None):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_features = n_features
+        self.root = None
+        
+    
+    def fit(self, X, y):
+        # grow the tree
+        self.n_features = X.shape[1] if not self.n_features else min(self.n_feats, X.shape[1])
+        self.root = self.grow_tree(X, y)
+    
+    def grow_tree(self, X, y, depth=0):
+        n_samples, n_feautres = X.shape
+        n_labels = len(np.unique(y))
+        
+        # stop
+        if (depth >= self.max_depth
+            or n_labels == 1
+            or n_samples < self.min_samples_split):
+            leaf_value = self.most_common_label(y)
+            return Node(value=leaf_value)
+
+        feat_idxs = np.random.choice(n_feautres, self.n_features, replace=False)
+        
+        # greedy search
+        best_feat, best_thresh = self.best_criteria(X, y, feat_idxs)
+        
+    
+    def best_criteria(self, X, y, feat_idxs):
+        best_gain = -1
+        split_idx, split_thresh = None, None
+        for feat_idx in feat_idxs:
+            X_column = X[:, feat_idx]
+            thresholds = np.unique(X_column)
+            for threshold in thresholds:
+                gain = self.information_gain(y, X_column, threshold)
+                
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feat_idx
+                    split_thresh = threshold
+        
+        return split_idx, split_thresh
+    
+    def information_gain(self, y, X_column, split_thresh):
+        # parent entropy
+        parent_entropy = entropy(y)
+        # generate split
+        left_idxs, right_idxs = self.split(X_column, split_thresh)
+        # weighted average child entropy
+        
+        # return information_gain
+    
+    def split(self, X_column, split_thresh):
+        left_idxs = np.argwhere(X_column <= split_thresh).flatten()
+        right_idxs = np.argwhere(X_column >= split_thresh).flatten()
+        return left_idxs, right_idxs           
+            
+    def most_common_label(self, y):
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
+    
+    
+    def predict(self, X):
+        
+        
         
